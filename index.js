c2d6b3d083b7e5a1cf485218ddc231f43328f76f
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2021-01-07 10:30:01 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ module.exports = {};
 module.exports.replaceAll = require("./src/replaceAll.js");
 module.exports.checkForDuplicates = require("./src/checkForDuplicates.js");
 module.exports.executeTemplateLiteral = require("./src/executeTemplateLiteral.js");
-module.exports.config = require("./src/config.js");
+module.exports.BaseConfig = require("./src/baseconfig.js");
